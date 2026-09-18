2c4fa578a77f271beaac06c8345ec12f834aed66
HarshVarshney0001 <harshvarshney8445@gmail.com>
2026-09-18 18:44:33 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -6,7 +6,6 @@ class TodoList:
         self.tasks.append(task)
 
     def remove_task(self, index):
-        # BUG: no check if index is out of range
         del self.tasks[index]
 
     def get_task(self, index):
@@ -16,12 +15,11 @@ class TodoList:
         self.tasks[index]["done"] = True
 
     def get_first_task(self):
-        # BUG: empty list not handled, will crash
         return self.tasks[0]
 
     def count_pending(self):
         count = 0
         for task in self.tasks:
-            if task["done"] = False:  # BUG: should be == not =
+            if task["done"] = False:
                 count += 1
         return count
